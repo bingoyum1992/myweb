faffe64e1ae4578f8ede91c8ea7b1a769352af9d
bingoyum1992 <bingoyum@163.com>
2019-03-28 16:52:15 +0800

    modified:   gcswr/src/components/pages/HeadNav.vue 	new file:   gcswr/src/components/pages/Piction.vue 	new file:   gcswr/src/components/pages/Sport.vue 	modified:   gcswr/src/router/index.js

--- gcswr/src/components/pages/HeadNav.vue
+++ gcswr/src/components/pages/HeadNav.vue
@@ -1,10 +1,10 @@
 <template>
     <el-container>
         <el-header>
-            <el-menu  background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" router=true>
+            <el-menu  background-color="#8f97ff" text-color="#fff" active-text-color="#ffd04b" :default-active= "activeIndex" class="el-menu-demo" mode="horizontal" :router= "true">
   <el-menu-item index="Langart">处理中心</el-menu-item>
-    <el-menu-item index="tyyd">订单管理</el-menu-item>
-  <el-menu-item index="shsy">订单管理</el-menu-item>
+    <el-menu-item index="Piction">订单管理</el-menu-item>
+  <el-menu-item index="Sport">订单管理</el-menu-item>
 </el-menu>
         </el-header>
     </el-container>
@@ -13,6 +13,12 @@
 <script>
   export default {
     name:'HeadNav',
+    data() {
+      return{
+        activeIndex: 'Langart'
+      }
+    }
+    
   }
 </script>
 
